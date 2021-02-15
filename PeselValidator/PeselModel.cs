35bcb60a53511e047fb051397bb665c6b7a6db67
Matijaz <matijaz69@gmail.com>
2021-02-15 14:41:22 +0100

    Add data structures

---
+++ PeselValidator/PeselModel.cs
@@ -0,0 +1,27 @@
+﻿namespace PeselValidator
+{
+    public enum Gender
+    {
+        Female,
+        Male,
+    }
+
+    public class PeselModel
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public int Number { get; set; }
+        public int CheckSum { get; set; }
+
+        public Gender GetSex()
+        {
+            return this.Number % 2 == 0 ? Gender.Female : Gender.Male;
+        }
+
+        public override string ToString()
+        {
+            return $"{Year:D2}{Month:D2}{Day:D2}{Number:D4}{CheckSum:D1}";
+        }
+    }
+}
---
+++ PeselValidator/PeselStruct.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace PeselValidator
+{
+    public class PeselStruct
+    {
+        public const int PeselSize = 11;
+
+        public PeselStruct(string pesel)
+        {
+            if (string.IsNullOrEmpty((this.Pesel)))
+                throw new NullReferenceException(nameof(pesel));
+
+            if (pesel.Length != PeselSize)
+                throw new ArgumentOutOfRangeException(nameof(pesel));
+
+            if (!int.TryParse(pesel, out int result))
+                throw new ArgumentException("The PESEL should be a number", nameof(pesel));
+
+            this.Pesel = pesel;
+        }
+
+        public string Pesel { get; private set; }
+
+        public int this[int index] => DigitAt(index);
+
+        public int Day => BirthDay();
+
+        public int Month => BirthMonth();
+
+        public int Year => BirthYear();
+
+        public int DigitAt(int index)
+        {
+            if (!string.IsNullOrEmpty((this.Pesel)))
+            {
+                if (index >= 0 && index < this.Pesel.Length)
+                {
+                    int result = (int) (this.Pesel[index] - '0');
+                    if (result < 0 || result > 9)
+                        throw new ArgumentOutOfRangeException(nameof(this.Pesel));
+                    return result;
+                }
+            }
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        public int BirthDay()
+        {
+            return 10 * DigitAt(4) + DigitAt(5);
+        }
+
+        public int BirthMonth()
+        {
+            int month = 10 * DigitAt(2) + DigitAt(3);
+            return Utils.GetPeselBirthMonth(month);
+        }
+
+        public int BirthYear()
+        {
+            int year = 10 * DigitAt(0) + DigitAt(1);
+            int month = 10 * DigitAt(2) + DigitAt(3);
+            return Utils.GetPeselBirthYear(year, month);
+        }
+    }
+}
